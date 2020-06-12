85e4c1c3db7b876966db387ac8acf957efb3340a
Sloan.Holliday <sloan.holliday@surescripts.com>
2020-06-12 06:59:01 -0400

    Updated comments and variable names.

--- source/domaindatalayer.jpa/src/main/java/com/mycompany/organizationdemo/domaindatalayer/jpa/repositories/DepartmentRepository.java
+++ source/domaindatalayer.jpa/src/main/java/com/mycompany/organizationdemo/domaindatalayer/jpa/repositories/DepartmentRepository.java
@@ -28,7 +28,7 @@ public class DepartmentRepository implements IDepartmentRepository {
 
   private final Logger logger;
   private final IDepartmentEntityDtoConverter deptConverter;
-  private final InternalDepartmentJpaRepository deptRepo;
+  private final InternalDepartmentJpaRepository internalDepartmentJpaRepository;
   private final EntityManager entityManager;
 
   /* The Inject annotation marks which constructor to use for IoC when there are multiple constructors */
@@ -37,7 +37,7 @@ public class DepartmentRepository implements IDepartmentRepository {
     this(LoggerFactory.getLogger(DepartmentRepository.class), em, deptConverter, deptRepo);
   }
 
-  public DepartmentRepository(Logger lgr, EntityManager em, IDepartmentEntityDtoConverter deptConverter, InternalDepartmentJpaRepository deptRepo) {
+  public DepartmentRepository(Logger lgr, EntityManager em, IDepartmentEntityDtoConverter deptConverter, InternalDepartmentJpaRepository internalDepartmentJpaRepository) {
     if (null == lgr) {
       throw new IllegalArgumentException("Logger is null");
     }
@@ -46,8 +46,8 @@ public class DepartmentRepository implements IDepartmentRepository {
       throw new IllegalArgumentException("IDepartmentEntityDtoConverter is null");
     }
 
-    if (null == deptRepo) {
-      throw new IllegalArgumentException("IDepartmentDomainData is null");
+    if (null == internalDepartmentJpaRepository) {
+      throw new IllegalArgumentException("InternalDepartmentJpaRepository is null");
     }
 
     if (null == em) {
@@ -55,15 +55,21 @@ public class DepartmentRepository implements IDepartmentRepository {
     }
 
     this.logger = lgr;
+
+    /* converter to convert jpa entities to dto objects */
     this.deptConverter = deptConverter;
-    this.deptRepo = deptRepo;
+
+    /* pass the simple crud stuff off to the InternalDepartmentJpaRepository */
+    this.internalDepartmentJpaRepository = internalDepartmentJpaRepository;
+
+    /* keep the EM around for more advanced query needs */
     this.entityManager = em;
   }
 
 
   @Override
   public Collection<DepartmentDto> findAll() {
-    List<DepartmentJpaEntity> entities = this.deptRepo.findAll();
+    List<DepartmentJpaEntity> entities = this.internalDepartmentJpaRepository.findAll();
     /* right here, desperately hoping for each DepartmentJpaEntity in the "entities" to NOT have employees hydrated */
     Collection<DepartmentDto> returnItems = this.deptConverter.convertToDtos(entities);
     return returnItems;
@@ -72,7 +78,7 @@ public class DepartmentRepository implements IDepartmentRepository {
   @Override
   public Optional<DepartmentDto> findById(long key) {
     Optional<DepartmentDto> returnItem = Optional.empty();
-    Optional<DepartmentJpaEntity> entity = this.deptRepo.findById(key);
+    Optional<DepartmentJpaEntity> entity = this.internalDepartmentJpaRepository.findById(key);
     if (entity.isPresent()) {
       returnItem = Optional.of(this.deptConverter.convertToDto(entity.get()));
     }
@@ -82,7 +88,7 @@ public class DepartmentRepository implements IDepartmentRepository {
   @Override
   public Optional<DepartmentDto> findByDepartmentName(String departmentName) {
     Optional<DepartmentDto> returnItem = Optional.empty();
-    Optional<DepartmentJpaEntity> entity = this.deptRepo.findDepartmentByDepartmentNameEquals(departmentName);
+    Optional<DepartmentJpaEntity> entity = this.internalDepartmentJpaRepository.findDepartmentByDepartmentNameEquals(departmentName);
     if (entity.isPresent()) {
       returnItem = Optional.of(this.deptConverter.convertToDto(entity.get()));
     }
@@ -91,7 +97,7 @@ public class DepartmentRepository implements IDepartmentRepository {
 
   @Override
   public Collection<DepartmentDto> findByCreateOffsetDateTimeBefore(OffsetDateTime zdt) {
-    Collection<DepartmentJpaEntity> entities = this.deptRepo.findByCreateOffsetDateTimeBefore(zdt);
+    Collection<DepartmentJpaEntity> entities = this.internalDepartmentJpaRepository.findByCreateOffsetDateTimeBefore(zdt);
     /* right here, desperately hoping for each DepartmentJpaEntity in the "entities" to NOT have employees hydrated */
     Collection<DepartmentDto> returnItems = this.deptConverter.convertToDtos(entities);
     return returnItems;
@@ -99,7 +105,7 @@ public class DepartmentRepository implements IDepartmentRepository {
 
   @Override
   public Collection<DepartmentDto> findBySurrogateKeyIn(Set<Long> departmentKeys) {
-    Collection<DepartmentJpaEntity> entities = this.deptRepo.findDepartmentByDepartmentKeyIn(departmentKeys);
+    Collection<DepartmentJpaEntity> entities = this.internalDepartmentJpaRepository.findDepartmentByDepartmentKeyIn(departmentKeys);
     /* right here, desperately hoping for each DepartmentJpaEntity in the "entities" to NOT have employees hydrated */
     Collection<DepartmentDto> returnItems = this.deptConverter.convertToDtos(entities);
     return returnItems;
@@ -108,14 +114,14 @@ public class DepartmentRepository implements IDepartmentRepository {
   @Override
   public DepartmentDto save(DepartmentDto item) {
     DepartmentJpaEntity entity = this.deptConverter.convertToEntity(item);
-    DepartmentJpaEntity savedEntity = this.deptRepo.save(entity);
+    DepartmentJpaEntity savedEntity = this.internalDepartmentJpaRepository.save(entity);
     DepartmentDto returnItem = this.deptConverter.convertToDto(savedEntity);
     return returnItem;
   }
 
   @Override
   public int deleteByKey(long departmentKey) {
-    int returnValue = this.deptRepo.deleteDepartmentByDepartmentKey(departmentKey);
+    int returnValue = this.internalDepartmentJpaRepository.deleteDepartmentByDepartmentKey(departmentKey);
     return returnValue;
   }
 
